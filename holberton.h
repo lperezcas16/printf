f10786aaa17de19c8edf32d5914389845f7cb8c1
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-18 17:37:48 +0000

    [upload] struct

--- holberton.h
+++ holberton.h
@@ -5,9 +5,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+* struct prnt - struct 
+* @flag: Binary Opytion
+* @prin_fun: pointer to flag
+*/
 
-
-
-
-
+tydef struct prnt
+{
+         char flag;
+         void (*prin_fun)();
+} print;
 #endif
