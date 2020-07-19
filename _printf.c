0dcb4304dae3d391610a3d715d5eab7bee30d111
valeria <valeria@kali>
2020-07-19 13:02:45 -0500

    refactor

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the function Cmatch that will
+ * determine which printing function to call depending on the conversion
+ * Return: buffer
+ */
+int _printf(const char *format, ...)
+{
+	char *buffer = NULL;
+	va_list args;
+	int i, j, Cmatch;
+	print match[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_integer},
+		{'i', print_integer},
+		{'o', print_octal},
+		{'h', print_hexa},
+		{'b', print_binary},
+		{'\0', NULL}
+	};
+	va_start(args, format);
+	buffer = malloc(1024);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	for (i = 0; format[i] != NULL; i++)
+	{
+		for (Cmatch = 0; match[Cmatch].flag != '\0'; Cmatch++)
+		{
+			if (format[i] == '%' && format[i + 1] == match[Cmatch].flag)
+			{
+				break;
+			}
+		}
+	}
+	for (j = 0; j < i; j++)
+	{
+		buffer[j] = format[i];
+	}
+	buffer[j] = '\0';
+	write(1, buffer, 1024);
+	va_end(args);
+	free(buffer);
+	return (buffer);
+}
