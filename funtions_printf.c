95274b0d2d713ac59bffbd2ff681a5170e8d4663
lperezcas16 <lperezcas@unbosque.edu.co>
2020-07-20 23:14:23 +0000

    [upload] funtions

--- funtions_printf.c
+++ funtions_printf.c
@@ -1,28 +1,36 @@
 #include "holberton.h"
-
-char *print_char(va_list a)
-{
-	
-}
 /**
-*return_binary - Funtion that return the decimal but in binary
+*print_binary - Funtion that return the decimal in binary
 *
-*@n: integer
+*@a: integer7
+*
 *Return: Char
 */
+char *print_binary(va_list a)
+{
+	char *ptn = return_binary(va_arg(a, int));
 
+	return (ptn);
+}
+/**
+*return_binary - Return the decimal in to binary number
+*
+*@n: integer to convert
+*
+*Return:  the number in binary
+*/
 char *return_binary(int n)
 {
 	char *new_space, *copy_a;
 	int i = 0, j, k, base = 2;
-	int f = n;
+	int number = n;
 
-	while (f > 0)
+	while (number > 0)
 	{
-		f /= base;
+		number /= base;
 		++i;
 	}
-	if (f == 0)
+	if (number == 0)
 	{
 		copy_a = "0";
 		return (copy_a);
@@ -35,11 +43,11 @@ char *return_binary(int n)
 		free(copy_a);
 		return (NULL);
 	}
-	i = 0, k = 0, f = n;
-	while (f > 0)
+	i = 0, k = 0, number = n;
+	while (number > 0)
 	{
-		new_space[i] = f % base + 48;
-		f /= base;
+		new_space[i] = number % base + 48;
+		numer /= base;
 		++i;
 	}
 	for (j = i - 1; j >= 0; j--, k++)
@@ -49,52 +57,43 @@ char *return_binary(int n)
 	copy_a[k] = '\0';
 	return (copy_a);
 }
-char *print_char(va_list a)
-{
-
-}
-char *print_string(va_list a)
-{
-
-}
-char *return_integer(int n)
-{
-
-}
-char *print_integer2(va_list, int count, int n)
-{
-
-}
-char *print_int(va_list a)
-{
-
-}
-char *print_binary(va_list a)
-{
-
-}
-char *print_hexa(va_list a)
-{
-
-}
-char *return_hexa(int n)
-{
-
-}
+/**
+*print_Hexa - Print the hexadecimal
+*
+*@a: The list
+*
+*Return: the pointer
+*/
 char *print_Hexa(va_list a)
 {
+	char *ptn = return_hexa(va_arg(a, int));
 
+	return (ptn);
 }
+/**
+*return_Hexa - convert a decimal in to hexadecimal
+*@n: integer to convert
+*
+*Return: the hexadecimal
+*/
 char *return_Hexa(int n)
 {
+	int decimalNumber, quotient;
+	int i = 0, j, temp;
+	char *hexadecimalNumber = malloc(sizeof(char) * (i + 2));
 
-}
-char *print_octal(va_list a)
-{
+	quotient = n;
 
-}
-char *return_octal(int n)
-{
-	
-}
+	while (quotient != 0)
+	{
 
+	temp = quotient % 16;
+	/*To convert integer into character*/
+		(temp < 10) ? (temp = temp + 48) :
+				(temp = temp + 55);
+	hexadecimalNumber[i++] = temp;
+	quotient = quotient / 16;
+	}
+	hexadecimalNumber[i] = '\0';
+	return (hexadecimalNumber);
+}
