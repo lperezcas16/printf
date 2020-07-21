b28fd5fd4b5cbbf3e86fdfc635faa56bf239da1c
valeria <valeria@kali>
2020-07-20 19:03:53 -0500

    print functions

---
+++ functions.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * print_char - prints char
+ * @a: char to concatenate to buffer
+ * Return: char to concatenate to buffer
+ */
+char *print_char (va_list a)
+{
+	char strg[2];
+	char *s;
+
+	strg[0] = va_arg(a, int);
+	strg[1] = '\0';
+	s = strg;
+	return (s);
+}
+/**
+ * print_string - print_string
+ * @a: to print
+ * Return: string
+ */
+char *print_string(va_list a)
+{
+	char *x = NULL;
+
+	x = va_arg(a, char *);
+	return (x);
+}
+/**
+ * print_int - prints integer
+ * @a: type of format to print
+ * Return: integer
+ */
+char *print_int(va_list a)
+{
+	char *x = return_integer(va_arg(a, int));
+
+	return (x);
+}
